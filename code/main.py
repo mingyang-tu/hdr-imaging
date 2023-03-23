c7e4ec946759a6bcfc4a78ccddd9982931dff66f
young <b07902058@ntu.edu.tw>
2023-03-23 15:30:24 +0800

    bug fix

--- code/main.py
+++ code/main.py
@@ -6,7 +6,7 @@ import cv2
 
 from lib import get_hdr
 from lib.MTB import *
-from lib.tonemap import tonemapping
+from lib.tonemap import tonemapping, local_tonemapping
 
 
 def parse_args() -> Namespace:
@@ -47,12 +47,13 @@ if __name__ == "__main__":
 
     images, delta_t = read_files(args)
 
-    sft_imgs = MTB(images[0], images[1:14])
+    sft_imgs = MTB(images[0], images[1:])
     sft_imgs.append(images[0])
 
     hdr_images = get_hdr(sft_imgs, delta_t, args.hdr_alg, args)
 
-    output = [tonemapping(img) for img in hdr_images]
+    # output = [tonemapping(img) for img in hdr_images]
+    output = [local_tonemapping(img) for img in hdr_images]
 
     if args.output_dir:
         cv2.imwrite(str(args.output_dir / "result.hdr"), hdr_images)
